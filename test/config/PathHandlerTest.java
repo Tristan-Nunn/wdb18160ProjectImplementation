3753934754426cf1efe84116673d19f61fe565cd
Tristan <wdb18160@uni.strath.ac.uk>
2020-06-25 23:02:50 +0100

    Pushing new branch

--- test/config/PathHandlerTest.java
+++ test/config/PathHandlerTest.java
@@ -1,8 +1,14 @@
 package config;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class PathHandlerTest
 {
-
+  @Test
+  public void testOneEqualsOne()
+  {
+    assertTrue(1==1);
+  }
 }
