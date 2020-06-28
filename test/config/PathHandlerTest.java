703432022112105a0a3f83f326516cce88807310
Tristan <wdb18160@uni.strath.ac.uk>
2020-06-29 00:07:03 +0100

    Added way more tests

--- test/config/PathHandlerTest.java
+++ test/config/PathHandlerTest.java
@@ -16,10 +16,24 @@ public class PathHandlerTest
     List<Ride> rides = RideHandler.generateRides("test/config/rideHandlerTestRaw.txt");
     Map<Ride, Map<Ride, Integer>> paths = PathHandler.generatePaths("test/config/pathHandlerTestRaw.txt", rides);
 
+    Ride testRide1 = rides.get(0);
+    Ride testRide2 = rides.get(1);
+
     assertTrue(paths.size() == 3);
-    assertEquals(2, paths.get(null).size());
-    assertEquals(1, paths.get(rides.get(0)).size());
-    assertEquals(0, paths.get(rides.get(1)).size());
+
+    Map<Ride, Integer> pathsFromEntrance = paths.get(null);
+    Map<Ride, Integer> pathsFromTest1 = paths.get(testRide1);
+    Map<Ride, Integer> pathsFromTest2 = paths.get(testRide2);
+
+    assertEquals(2, pathsFromEntrance.size());
+    assertEquals(1, pathsFromTest1.size());
+    assertEquals(1, pathsFromTest2.size());
+
+    assertEquals(200, (long)pathsFromEntrance.get(testRide1));
+    assertEquals(10000, (long)pathsFromEntrance.get(testRide2));
+
+    assertEquals(100, (long)pathsFromTest1.get(testRide2));
+    assertEquals(100, (long)pathsFromTest2.get(testRide1));
   }
 
 }
